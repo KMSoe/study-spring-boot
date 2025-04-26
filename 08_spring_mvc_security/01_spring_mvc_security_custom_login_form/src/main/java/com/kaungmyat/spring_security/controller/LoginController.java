c033e6dec56f6df2757587bebf866b018682f3a5
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2025-04-26 22:47:44 +0630

    add: Spring MVC Security

---
+++ 08_spring_mvc_security/01_spring_mvc_security_custom_login_form/src/main/java/com/kaungmyat/spring_security/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.kaungmyat.spring_security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String showMyLoginPage() {
+
+//         return "plain-login";
+        return "fancy-login";
+    }
+}
